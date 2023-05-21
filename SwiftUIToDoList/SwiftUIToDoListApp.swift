4ba0b82fca567d68379d1529b812097b4e9ae826
Kirill Khomicevich <khomicevichkirill@icloud.com>
2023-05-21 16:27:26 +0300

    Initial Commit

---
+++ SwiftUIToDoList/SwiftUIToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToDoListApp.swift
+//  SwiftUIToDoList
+//
+//  Created by Kirill Khomytsevych on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
